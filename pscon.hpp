a95f2364d9dd56db6868239944118fff7cbfc16d
nOOb3167 <nOOb3167@gmail.com>
2019-05-16 09:06:35 +0200

    test_nupd: misc

--- pscon.hpp
+++ pscon.hpp
@@ -34,7 +34,7 @@ class PsCon
 {
 public:
 	inline virtual ~PsCon() {};
-	inline virtual res_t reqPost(const std::string &path, const std::string &data) = 0;
+	inline virtual res_t req(const std::string &path, const std::string &data) = 0;
 	
 	ConProgress m_prog;
 };
@@ -69,9 +69,9 @@ public:
 	}
 
 	inline res_t
-	reqPost_(const std::string &path, const std::string &data)
+	req_(const http::verb &verb, const std::string &path, const std::string &data)
 	{
-		http::request<http::string_body> req(http::verb::post, m_host_http_rootpath + path, 11);
+		http::request<http::string_body> req(verb, m_host_http_rootpath + path, 11);
 		req.set(http::field::host, m_host_http);
 		req.set(http::field::user_agent, BOOST_BEAST_VERSION_STRING);
 		http::write(*m_socket, req);
@@ -91,10 +91,10 @@ public:
 	}
 
 	inline virtual res_t
-	reqPost(const std::string &path, const std::string &data) override
+	req(const std::string &path, const std::string &data) override
 	{
 		m_prog.onRequest(path, data);
-		res_t res = reqPost_(path, data);
+		res_t res = req_(http::verb::get, path, data);
 		if (res.result_int() != 200)
 			throw std::runtime_error("");
 		return res;
@@ -126,7 +126,7 @@ public:
 	}
 
 	inline virtual res_t
-	reqPost(const std::string &path, const std::string &data) override
+	req(const std::string &path, const std::string &data) override
 	{
 		m_prog.onRequest(path, data);
 		return res_t(boost::beast::http::status::ok, 11, std::string());
