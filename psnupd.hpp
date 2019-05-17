836a5bc80dc8a4d91b2f43f76bd0234887f2b2f6
nOOb3167 <nOOb3167@gmail.com>
2019-05-17 05:22:40 +0200

    test_nupd: misc

--- psnupd.hpp
+++ psnupd.hpp
@@ -1,6 +1,3 @@
-#define BOOST_TEST_MODULE nupd
-#include <boost/test/included/unit_test.hpp>
-
 #include <cassert>
 #include <cstdlib>
 #include <algorithm>
@@ -20,11 +17,7 @@
 
 // boost::filesystem::{weakly_}canonical : weakly does not require existence
 
-using fpt_t = std::tuple<boost::filesystem::path, std::string>;
-using fpt3_t = std::tuple<std::vector<fpt_t>, std::vector<fpt_t>, std::vector<fpt_t> >;
 using ps_sha_t = std::string;
-
-const char ps_uniq_path_pattern[] = "pstmp%%%%-%%%%-%%%%-%%%%";
 
 template<typename T, typename U>
 class ItPair
@@ -85,12 +78,10 @@ public:
 		const std::vector<ps_sha_t> &goal_sums)
 	{
 		dd_t dd;
-		assert(beg_fils.size() == beg_sums.size());
-		assert(goal_fils.size() == goal_sums.size());
-		for (size_t i = 0; i < goal_fils.size(); i++)
-			dd[goal_fils[i]].m_a = goal_sums[i];
-		for (size_t i = 0; i < beg_fils.size(); i++)
-			dd[beg_fils[i]].m_b = beg_sums[i];
+		for (const auto &[k, v] : ItPair(goal_fils, goal_sums))
+			dd[k].m_a = v;
+		for (const auto &[k, v] : ItPair(beg_fils, beg_sums))
+			dd[k].m_b = v;
 		return dd;
 	}
 
@@ -189,12 +180,13 @@ _del_last_if_file(const boost::filesystem::path &path)
 inline void
 _tmp_copy_force_makedst(const boost::filesystem::path &src, const boost::filesystem::path &dst_)
 {
+	const char uniq_path_pattern[] = "pstmp%%%%-%%%%-%%%%-%%%%";
 	const auto &dst = boost::filesystem::weakly_canonical(dst_);
 	assert(dst.has_parent_path());
 	_del_last_if_file(dst);
 	boost::filesystem::create_directories(dst.parent_path());
 	if (boost::filesystem::exists(dst))
-		boost::filesystem::rename(dst, boost::filesystem::temp_directory_path() / boost::filesystem::unique_path(ps_uniq_path_pattern));
+		boost::filesystem::rename(dst, boost::filesystem::temp_directory_path() / boost::filesystem::unique_path(uniq_path_pattern));
 	boost::filesystem::copy_file(src, dst, boost::filesystem::copy_option::fail_if_exists);
 }
 
@@ -259,129 +251,3 @@ _main(const boost::filesystem::path &ourroot, const boost::filesystem::path &the
 
 	return EXIT_SUCCESS;
 }
-
-class TmpDirX
-{
-public:
-	inline TmpDirX() :
-		m_d(boost::filesystem::temp_directory_path() / boost::filesystem::unique_path(ps_uniq_path_pattern))
-	{
-		boost::filesystem::create_directories(m_d);
-	}
-
-	inline ~TmpDirX()
-	{
-		if (boost::filesystem::is_directory(m_d))
-			boost::filesystem::remove_all(m_d);
-	}
-
-	boost::filesystem::path m_d;
-};
-
-class TmpDirFixture
-{
-public:
-	inline TmpDirFixture(const fpt3_t &fpt) :
-		TmpDirFixture(std::get<0>(fpt), std::get<1>(fpt), std::get<2>(fpt))
-	{}
-
-	inline TmpDirFixture(
-		const std::vector<fpt_t> &fpt_our,
-		const std::vector<fpt_t> &fpt_the,
-		const std::vector<fpt_t> &fpt_fin
-	) :
-		m_tmpd_our(),
-		m_tmpd_the(),
-		m_fpt_our(fpt_our),
-		m_fpt_the(fpt_the),
-		m_fpt_fin(fpt_fin)
-	{
-		_filldir(m_tmpd_our.m_d, fpt_our);
-		_filldir(m_tmpd_the.m_d, fpt_the);
-	}
-
-	inline virtual ~TmpDirFixture()
-	{
-		for (const auto &[k, v] : m_fpt_fin)
-			BOOST_REQUIRE(_readfile(m_tmpd_our.m_d / k) == v);
-	}
-
-	inline static void
-	_filldir(const boost::filesystem::path &tmpd, const std::vector<fpt_t> &fpt)
-	{
-		for (auto &[k, v] : fpt) {
-			BOOST_REQUIRE(k.is_relative() && k == k.lexically_normal() && (tmpd / k).has_parent_path());
-			boost::filesystem::create_directories((tmpd / k).parent_path());
-			if (!boost::filesystem::ofstream((tmpd / k), std::ios_base::out | std::ios_base::binary).write(v.data(), v.size()).good())
-				throw std::runtime_error("");
-		}
-	}
-
-	inline static std::string
-	_readfile(const boost::filesystem::path &path)
-	{
-		std::stringstream buf;
-		buf << boost::filesystem::ifstream(path, std::ios_base::in | std::ios_base::binary).rdbuf();
-		return buf.str();
-	}
-
-	TmpDirX m_tmpd_our;
-	TmpDirX m_tmpd_the;
-	std::vector<fpt_t> m_fpt_our;
-	std::vector<fpt_t> m_fpt_the;
-	std::vector<fpt_t> m_fpt_fin;
-};
-
-BOOST_AUTO_TEST_SUITE(nupd_suite);
-
-BOOST_AUTO_TEST_CASE(nupd_main0)
-{
-	TmpDirFixture w(
-		{ {"a.txt", "a"} },
-		{ {"a.txt", "a"} },
-		{ {"a.txt", "a"} }
-	);
-	_main(w.m_tmpd_our.m_d, w.m_tmpd_the.m_d);
-}
-
-BOOST_AUTO_TEST_CASE(nupd_main1)
-{
-	TmpDirFixture w(
-		{},
-		{ {"a.txt", "a"} },
-		{ {"a.txt", "a"} }
-	);
-	_main(w.m_tmpd_our.m_d, w.m_tmpd_the.m_d);
-}
-
-BOOST_AUTO_TEST_CASE(nupd_main2)
-{
-	TmpDirFixture w(
-		{ {"a.txt", "a"} },
-		{ {"a.txt", "b"} },
-		{ {"a.txt", "b"} }
-	);
-	_main(w.m_tmpd_our.m_d, w.m_tmpd_the.m_d);
-}
-
-BOOST_AUTO_TEST_CASE(nupd_main3)
-{
-	TmpDirFixture w(
-		{ {"a0.txt", "c"}, {"a1.txt", "c"} },
-		{ {"a.txt", "b"} },
-		{ {"a.txt", "b"}, {"a1.txt", "c" } }
-	);
-	_main(w.m_tmpd_our.m_d, w.m_tmpd_the.m_d);
-}
-
-BOOST_AUTO_TEST_CASE(nupd_main4)
-{
-	TmpDirFixture w(
-		{ {"a0.txt", "c"} },
-		{ {"a.txt", "b"}, {"b.txt", "c"} },
-		{ {"a0.txt", "c"}, {"a.txt", "b"}, {"b.txt", "c"} }
-	);
-	_main(w.m_tmpd_our.m_d, w.m_tmpd_the.m_d);
-}
-
-BOOST_AUTO_TEST_SUITE_END();
